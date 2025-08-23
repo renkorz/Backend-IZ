6ec1074a5ae36217471f4a0ca42c1959e50980c5
renkorz <zaarate7ignacio@gmail.com>
2025-08-23 13:32:33 -0400

    Integración principal a repositorio.

---
+++ nsqk/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NsqkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nsqk'
---
+++ nsqk/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Nacionalidad(models.Model):
+    pais = models.CharField(max_length=50, null=False)
+    nacionalidad = models.CharField(max_length=50, null=False)
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=250, null=False)
+    pseudonimo = models.CharField(max_length=50, null=True)
+    id_nacionalidad = models.ForeignKey(Nacionalidad, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+class Comuna(models.Model):
+    codigo = models.CharField(max_length=5, null=False)
+    comuna = models.CharField(max_length=50, null=False)
+
+class Direccion(models.Model):
+    id_comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
+    calle = models.CharField(max_length=50, null=True)
+    numero = models.CharField(max_length=10, null=True)
+    departamento = models.CharField(max_length=10, null=True)
+
+class Libro(models.Model):
+    id_biblioteca = models.ForeignKey(Biblioteca, on_delete=models.CASCADE)
+    genero = models.CharField(max_length=50, )
