680de8984cb608d5c79270a0b2e6a52964458472
renkorz <zaarate7ignacio@gmail.com>
2025-11-05 22:34:21 -0300

    Actualizado (en base a modelos)

--- nsqk/admin.py
+++ nsqk/admin.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
-from .models import (
-    Nacionalidad,
-    Autor,
-    Comuna,
-    Direccion,
-    Biblioteca,
-    Libro,
-    Lector,
-    Prestamo
-)
+from .models import (Nacionalidad, Autor, Comuna, Direccion, Biblioteca, Libro, Lector, Prestamo, TipoCategoria, TipoParametro, Parametro, Categoria, Reserva)
 
 # Register your models here.
 admin.site.register(Nacionalidad)
@@ -19,3 +10,8 @@ admin.site.register(Biblioteca)
 admin.site.register(Libro)
 admin.site.register(Lector)
 admin.site.register(Prestamo)
+admin.site.register(Parametro)
+admin.site.register(TipoParametro)
+admin.site.register(Categoria)
+admin.site.register(TipoCategoria)
+admin.site.register(Reserva)
