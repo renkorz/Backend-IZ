f272fb8eb18382b86c6dc2ffc8bb6b88c11240b5
renkorz <zaarate7ignacio@gmail.com>
2025-11-05 22:31:47 -0300

    URL's actualizadas (modelos updated)

--- nsqk/urls.py
+++ nsqk/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import (
-    NacionalidadViewSet, AutorViewSet, ComunaViewSet,
-    DireccionViewSet, BibliotecaViewSet, LibroViewSet,
-    LectorViewSet, PrestamoViewSet
-)
+from .views import (NacionalidadViewSet, AutorViewSet, ComunaViewSet, DireccionViewSet, BibliotecaViewSet, LibroViewSet, LectorViewSet, PrestamoViewSet, CategoriaViewSet, TipoCategoriaViewSet, ParametroViewSet, TipoParametroViewSet, ReservaViewSet)
 
 router = routers.DefaultRouter()
 router.register(r'nacionalidades', NacionalidadViewSet)
@@ -15,6 +11,11 @@ router.register(r'bibliotecas', BibliotecaViewSet)
 router.register(r'libros', LibroViewSet)
 router.register(r'lectores', LectorViewSet)
 router.register(r'prestamos', PrestamoViewSet)
+router.register(r'categoria', CategoriaViewSet)
+router.register(r'tipocategoria', TipoCategoriaViewSet)
+router.register(r'parametro', ParametroViewSet)
+router.register(r'tipoparametro', TipoParametroViewSet)
+router.register(r'reserva', ReservaViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
