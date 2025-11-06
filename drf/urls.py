27a3451c06ecb59068bc97daf96a587f6e38c7a8
renkorz <zaarate7ignacio@gmail.com>
2025-11-05 21:45:10 -0300

    Integración de redireccionamientos.

--- drf/urls.py
+++ drf/urls.py
@@ -14,13 +14,46 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from nsqk import views
 from django.contrib import admin
 from django.urls import path, include
-from nsqk import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.contrib.auth import views as auth_views
+
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Documentación API, NSQK',
+        default_version='v1',
+        description='NSQK, aplicación en DJANGO',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='user@test.com'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nsqk/', include('nsqk.urls')),
-    path('',views.pagina_inicio, name='home')
-    # path('nsqk/', views.pagina_inicio)
+    
+    path('',views.pagina_inicio, name='pagina_inicio'),
+    path('nsqk/', include('nsqk.urls')), # URL's para aplicación principal
+    
+    #URL's vistas de lista
+    path('listado_autores', views.listado_autores, name='listado_autores'),
+    path('listado_comunas', views.listado_comunas, name='listado_lectores'),
+    path('listado_lectores', views.listado_lectores, name='listado_lectres'),
+    path('listado_libros', views.listado_libros, name='lsitado_libros'),
+    
+    # URL's para documentación de API
+    path('apidocs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
+    # URL's de autenticación
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LoginView.as_view(), name='logout'),
+    path('registro/', views.registro, name='registro'),
 ]
